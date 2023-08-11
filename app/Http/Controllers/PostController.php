d91d611a4b30493b30a9764a25e78d8c0d7d44da
iqbalthex <iqbalariem232@gmail.com>
2023-08-11 19:36:21 +0700

    Add relations to Post model

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,62 +4,58 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\View\View;
 
-class PostController extends Controller
-{
+class PostController extends Controller {
     /**
      * Display a listing of the resource.
      */
-    public function index()
-    {
-        //
+    public function index(Request $request): View {
+        $posts = Post::all();
+
+        return view('posts.index', compact());
     }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
-    {
+    public function create(): View {
         //
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
-    {
+    public function store(Request $request): RedirectResponse {
         //
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Post $post)
-    {
+    public function show(Post $post): View {
         //
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Post $post)
-    {
+    public function edit(Post $post): View {
         //
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Post $post)
-    {
+    public function update(Request $request, Post $post): RedirectResponse {
         //
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Post $post)
-    {
+    public function destroy(Post $post): RedirectResponse {
         //
     }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,10 +2,23 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\ {
+  Factories\HasFactory,
+  Model,
+  Relations\BelongsTo,
+  Relations\HasMany,
+};
 
-class Post extends Model
-{
+class Post extends Model {
     use HasFactory;
+
+    protected $guarded = ['id'];
+
+    public function comments(): BelongsTo {
+        return $this->belongsTo(User::class);
+    }
+
+    public function comments(): HasMany {
+        return $this->hasMany(Comment::class);
+    }
 }
