f48c2cff89b783a02e34e118ff7cd9d92df5c255
iqbalthex <iqbalariem232@gmail.com>
2023-08-23 20:00:28 +0700

    Extract sidebar itself to component

---
+++ resources/js/Components/Sidebar.vue
@@ -0,0 +1,172 @@
+<script setup>
+
+defineProps({
+  sidebarOpen:   { type: Boolean, required: true },
+  changeCurrent: { type: Function },
+  current:       { type: String }
+});
+
+import DropdownGroup from '@/Components/DropdownGroup.vue';
+import NavLink       from '@/Components/NavLink.vue';
+
+import BookIcon      from '@/Components/Icons/BookIcon.vue';
+import BuildingIcon  from '@/Components/Icons/BuildingIcon.vue';
+import FileLinesIcon from '@/Components/Icons/FileLinesIcon.vue';
+import FilePenIcon   from '@/Components/Icons/FilePenIcon.vue';
+import HomeIcon      from '@/Components/Icons/HomeIcon.vue';
+import LogoutIcon    from '@/Components/Icons/ArrowRightBracketIcon.vue';
+import StarIcon        from '@/Components/Icons/StarIcon.vue';
+import TableColumnIcon from '@/Components/Icons/TableColumnIcon.vue';
+import UsersIcon       from '@/Components/Icons/UsersIcon.vue';
+
+const studies = [
+  'Bahasa Indonesia',
+  'Matematika',
+  'IPAS',
+  'Pendidikan Pancasila',
+  'Bahasa Inggris',
+  'Seni Budaya',
+];
+
+</script>
+
+<template>
+<slot name="sidebar">
+  <nav :class="sidebarOpen ? 'min-w-60' : 'min-w-14'"
+    class="nav-sidebar overflow-x-hidden">
+    <div class="sidebar min-h-screen max-h-screen bg-violet-600 overflow-y-scroll">
+      <!-- Logo -->
+      <div class="pl-2">
+        <Nav-Link
+          @click="changeCurrent('dashboard')">
+          <HomeIcon #icon />
+          Raport
+        </Nav-Link>
+      </div>
+
+      <div class="pl-2 py-3 border-t border-gray-200">
+        <!-- "route().current('dashboard')" -->
+
+        <Nav-Link
+          :is-active="current === 'dashboard'"
+          @click="changeCurrent('dashboard')">
+          <TableColumnIcon #icon />
+          Dashboard
+        </Nav-Link>
+
+        <Nav-Link
+          :is-active="current === 'school'"
+          @click="changeCurrent('school')">
+          <BuildingIcon #icon />
+          Data Sekolah
+        </Nav-Link>
+
+        <Nav-Link
+          :is-active="current === 'students'"
+          @click="changeCurrent('students')">
+          <UsersIcon #icon />
+          Data Siswa
+        </Nav-Link>
+
+        <Nav-Link
+          :is-active="current === 'objective'"
+          @click="changeCurrent('objective')">
+          <BookIcon #icon />
+          Tujuan Pembelajaran
+        </Nav-Link>
+
+        <Nav-Link
+          :is-active="current === 'extra'"
+          @click="changeCurrent('extra')">
+          <StarIcon #icon />
+          Ekstrakurikuler
+        </Nav-Link>
+
+        <Dropdown-Group
+          :is-active="current.startsWith('formative')"
+          :item-count="studies.length">
+          <template #dropdown-trigger>
+            <Nav-Link
+              :is-active="current.startsWith('formative')"
+              @click="changeCurrent('formative')"
+              class="dropdown-trigger">
+              <FilePenIcon #icon />
+              Penilaian Formatif
+            </Nav-Link>
+          </template>
+          
+
+          <template #dropdown-items>
+            <Nav-Link
+              v-for="study, index in studies"
+              :key="index"
+              :is-active="current === `formative.${study}`"
+              @click="changeCurrent(`formative.${study}`)"
+              class="dropdown-item">
+              <FilePenIcon #icon />
+              {{ study }}
+            </Nav-Link>
+          </template>
+        </Dropdown-Group>
+
+
+        <Dropdown-Group
+          :is-active="current.startsWith('sumative')"
+          :item-count="studies.length">
+          <template #dropdown-trigger>
+            <Nav-Link
+              :is-active="current.startsWith('sumative')"
+              @click="changeCurrent('sumative')"
+              class="dropdown-trigger">
+              <FilePenIcon #icon />
+              Penilaian Sumatif
+            </Nav-Link>
+          </template>
+
+          <template #dropdown-items>
+            <Nav-Link
+              v-for="study, index in studies"
+              :key="index"
+              :is-active="current === `sumative.${study}`"
+              @click="changeCurrent(`sumative.${study}`)"
+              class="dropdown-item">
+              <FilePenIcon #icon />
+              {{ study }}
+            </Nav-Link>
+          </template>
+        </Dropdown-Group>
+
+        <Nav-Link
+          :is-active="current === 'print.raport'"
+          @click="changeCurrent('print.raport')">
+          <FileLinesIcon #icon />
+          Cetak Raport
+        </Nav-Link>
+      </div>
+
+      <div class="pl-2 pt-3 border-t border-gray-200">
+        <Nav-Link>
+          <LogoutIcon #icon class="rotate-180" />
+          <form @submit.prevent="submit">
+            <button type="submit">
+              Log Out
+            </button>
+          </form>
+        </Nav-Link>
+      </div>
+    </div>
+  </nav>
+</slot>
+</template>
+
+<style scoped>
+
+.nav-sidebar {
+  transition: min-width .3s;
+}
+
+.nav-sidebar > .sidebar::-webkit-scrollbar {
+  @apply hidden;
+}
+
+</style>
--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -1,36 +1,20 @@
 <script setup>
 
-import { computed, ref } from 'vue';
-import { useForm } from '@inertiajs/vue3';
+import { ref } from 'vue';
 
-import DropdownGroup from '@/Components/DropdownGroup.vue';
-import NavLink       from '@/Components/NavLink.vue';
+import Sidebar from '@/Components/Sidebar.vue';
 
 import AngleLeftIcon  from '@/Components/Icons/AngleLeftIcon.vue';
 import AngleRightIcon from '@/Components/Icons/AngleRightIcon.vue';
-import BookIcon       from '@/Components/Icons/BookIcon.vue';
-import BuildingIcon  from '@/Components/Icons/BuildingIcon.vue';
-import FileLinesIcon from '@/Components/Icons/FileLinesIcon.vue';
-import FilePenIcon   from '@/Components/Icons/FilePenIcon.vue';
-import HomeIcon      from '@/Components/Icons/HomeIcon.vue';
-import LogoutIcon    from '@/Components/Icons/ArrowRightBracketIcon.vue';
-import StarIcon        from '@/Components/Icons/StarIcon.vue';
-import TableColumnIcon from '@/Components/Icons/TableColumnIcon.vue';
-import UsersIcon       from '@/Components/Icons/UsersIcon.vue';
 
 const sidebarOpen = ref(true);
 const hambButton  = ref(null);
 const current     = ref('dashboard');
 
-const studies = [
-  'Bahasa Indonesia',
-  'Matematika',
-  'IPAS',
-  'Pendidikan Pancasila',
-  'Bahasa Inggris',
-  'Seni Budaya',
-];
 
+function changeCurrent(name) {
+  current.value = name;
+}
 
 function submit() {
   // form.post(route('logout'));
@@ -54,136 +38,10 @@ function closeSidebar({ clientX, clientY }) {
 <div>
   <div class="flex min-h-screen bg-gray-50">
     <slot name="sidebar">
-      <nav :class="sidebarOpen ? 'min-w-60' : 'min-w-14'"
-        class="nav-sidebar overflow-x-hidden">
-        <div class="min-h-screen max-h-screen bg-violet-600 overflow-y-scroll">
-          <!-- Logo -->
-          <div class="pl-2">
-            <NavLink
-              :href="route('dashboard')">
-              <HomeIcon #icon />
-              Raport
-            </NavLink>
-          </div>
-
-          <div class="pl-2 py-3 border-t border-gray-200">
-            <!-- "route().current('dashboard')" -->
-
-            <NavLink
-              :href="route('dashboard')"
-              :is-active="current === 'dashboard'"
-              @click="current = 'dashboard'">
-              <TableColumnIcon #icon />
-              Dashboard
-            </NavLink>
-
-            <NavLink
-              :href="route('profile.edit')"
-              :is-active="current === 'school'"
-              @click="current = 'school'">
-              <BuildingIcon #icon />
-              Data Sekolah
-            </NavLink>
-
-            <NavLink
-              :href="route('profile.edit')"
-              :is-active="current === 'students'"
-              @click="current = 'students'">
-              <UsersIcon #icon />
-              Data Siswa
-            </NavLink>
-
-            <NavLink
-              :href="route('profile.edit')"
-              :is-active="current === 'objective'"
-              @click="current = 'objective'">
-              <BookIcon #icon />
-              Tujuan Pembelajaran
-            </NavLink>
-
-            <NavLink
-              :href="route('profile.edit')"
-              :is-active="current === 'extra'"
-              @click="current = 'extra'">
-              <StarIcon #icon />
-              Ekstrakurikuler
-            </NavLink>
-
-            <Dropdown-Group
-              :is-active="current.startsWith('formative')"
-              :item-count="studies.length">
-              <template #dropdown-trigger>
-                <NavLink
-                  :is-active="current.startsWith('formative')"
-                  @click="current = 'formative'"
-                  class="dropdown-trigger">
-                  <FilePenIcon #icon />
-                  Penilaian Formatif
-                </NavLink>
-              </template>
-              
-
-              <template #dropdown-items>
-                <NavLink
-                  v-for="study, index in studies"
-                  :key="index"
-                  :is-active="current === `formative.${study}`"
-                  @click="current = `formative.${study}`"
-                  class="dropdown-item">
-                  <FilePenIcon #icon />
-                  {{ study }}
-                </NavLink>
-              </template>
-            </Dropdown-Group>
-
-
-            <Dropdown-Group
-              :is-active="current.startsWith('sumative')"
-              :item-count="studies.length">
-              <template #dropdown-trigger>
-                <NavLink
-                  :is-active="current.startsWith('sumative')"
-                  @click="current = 'sumative'"
-                  class="dropdown-trigger">
-                  <FilePenIcon #icon />
-                  Penilaian Sumatif
-                </NavLink>
-              </template>
-
-              <template #dropdown-items>
-                <NavLink
-                  v-for="study, index in studies"
-                  :key="index"
-                  :is-active="current === `sumative.${study}`"
-                  @click="current = `sumative.${study}`"
-                  class="dropdown-item">
-                  <FilePenIcon #icon />
-                  {{ study }}
-                </NavLink>
-              </template>
-            </Dropdown-Group>
-
-            <NavLink
-              :href="route('profile.edit')"
-              :is-active="current === 'profile7'"
-              @click="current = 'profile7'">
-              <FileLinesIcon #icon />
-              Cetak Raport
-            </NavLink>
-          </div>
-
-          <div class="pl-2 pt-3 border-t border-gray-200">
-            <NavLink>
-              <LogoutIcon #icon class="rotate-180" />
-              <form @submit.prevent="submit">
-                <button type="submit">
-                  Log Out
-                </button>
-              </form>
-            </NavLink>
-          </div>
-        </div>
-      </nav>
+      <Sidebar
+        :sidebarOpen="sidebarOpen"
+        :changeCurrent="changeCurrent"
+        :current="current" />
     </slot>
 
     <div :class="sidebarOpen && 'open'" @click="closeSidebar"
@@ -213,10 +71,6 @@ function closeSidebar({ clientX, clientY }) {
 
 <style scoped>
 
-.nav-sidebar {
-  transition: min-width .3s;
-}
-
 .main-container {
   max-width: calc(100% - 3.5rem);
   transition: max-width .3s;
@@ -224,10 +78,6 @@ function closeSidebar({ clientX, clientY }) {
 
 .main-container.open {
   max-width: calc(100% - 15rem);
-}
-
-.overflow-y-scroll::-webkit-scrollbar {
-  @apply hidden;
 }
 
 </style>
