f388318a48d5019023ede7dbaec1f52646343a35
iqbalthex <iqbalariem232@gmail.com>
2023-09-10 08:49:07 +0700

    Update Students-Page

--- resources/js/Pages/Teacher/Students-Page.vue
+++ resources/js/Pages/Teacher/Students-Page.vue
@@ -13,156 +13,184 @@ import { computed, ref } from 'vue';
 import { moveUp, moveDown, moveLeft, moveRight } from '@/utils/input-methods.js';
 
 import FrozenTable from '@/Components/FrozenTable.vue';
+import Loading     from '@/Components/Loading.vue';
 
-const scores = ref([72, 76, 73, 75, 80]);
+const students = computed(() => props.students.value[props.currentClassroom.value]);
+const loading = computed(() => !props.students.value[props.currentClassroom.value]?.length);
 
 </script>
 
 <template>
-<section>
 
-<div class="mx-auto max-w-screen-xl shadow-md">
-  <div class="max-w-screen max-h-screen bg-[#999] dark:bg-gray-800 rounded">
-    <div class="table-wrapper w-full h-[500px] overflow-y-scroll">
-      <Frozen-Table
-        :items="students.value[currentClassroom.value]">
-        <template #head-start>
-          <th class="w-12" scope="col">No</th>
-          <th class="w-full" scope="col">Nama</th>
-        </template>
+<Frozen-Table>
+  <template #head>
+    <tr>
+      <div class="freeze">
+        <th class="w-12" scope="col">No</th>
+        <th class="w-full" scope="col">Nama</th>
+      </div>
+      <th scope="col">NISN</th>
+      <th scope="col">L/P</th>
+      <th scope="col">Tempat Lahir</th>
+      <th scope="col">Tanggal Lahir</th>
+      <th scope="col">Agama</th>
+      <th scope="col">Alamat</th>
+      <th scope="col" class="parent">Nama Ayah</th>
+      <th scope="col" class="parent">Nama Ibu</th>
+      <th scope="col" class="parent">Pekerjaan Ayah</th>
+      <th scope="col" class="parent">Pekerjaan Ibu</th>
+      <th scope="col" class="parent">Alamat Orang Tua</th>
+      <th scope="col" class="parent">No Handphone Orang Tua</th>
+      <th scope="col">Nama Wali</th>
+      <th scope="col">Pekerjaan Wali</th>
+      <th scope="col">Alamat Wali</th>
+      <th scope="col">No Handphone Wali</th>
+      <div class="freeze">
+        <th scope="col" class="w-8">S</th>
+        <th scope="col" class="w-8">I</th>
+        <th scope="col" class="w-8">A</th>
+      </div>
+    </tr>
+  </template>
 
-        <template #head-center>
-          <th scope="col">NISN</th>
-          <th scope="col">L/P</th>
-          <th scope="col">Tempat Lahir</th>
-          <th scope="col">Tanggal Lahir</th>
-          <th scope="col">Agama</th>
-          <th scope="col">Alamat</th>
-          <th scope="col" class="parent">Nama Ayah</th>
-          <th scope="col" class="parent">Nama Ibu</th>
-          <th scope="col" class="parent">Pekerjaan Ayah</th>
-          <th scope="col" class="parent">Pekerjaan Ibu</th>
-          <th scope="col" class="parent">Alamat Orang Tua</th>
-          <th scope="col" class="parent">No Handphone Orang Tua</th>
-          <th scope="col">Nama Wali</th>
-          <th scope="col">Pekerjaan Wali</th>
-          <th scope="col">Alamat Wali</th>
-          <th scope="col">No Handphone Wali</th>
-        </template>
-
-        <template #head-end>
-          <th scope="col" class="w-8 presence">S</th>
-          <th scope="col" class="w-8 presence">I</th>
-          <th scope="col" class="w-8 presence">A</th>
-        </template>
-
-        <template #body-start="{ item, index }">
-          <th class="w-12" scope="row">{{ index + 1 }}</th>
+  <template #body>
+    <tr v-if="loading">
+      <td :colspan="10"><Loading /></td>
+    </tr>
+    <template v-else>
+      <tr v-for="student, rowIndex in students"
+        :data-row="rowIndex"
+        :key="student.id"
+        class="border-b border-slate-400 dark:border-gray-700 tes">
+        <div class="freeze bg-gray-100">
+          <th class="w-12" scope="row">{{ rowIndex + 1 }}</th>
           <td class="w-full text-left border-l">
+            <!-- <Movable-Input></Movable-Input> -->
             <input
               data-col="0"
-              :data-row="index"
-              :value="item.name"
+              :data-row="rowIndex"
+              :value="student.name"
               @keydown.prevent.up="moveUp($event)"
-              @keydown.prevent.down="moveDown($event)"
+              @keydown.prevent.down="moveDown($event, (rowIndex === students.length - 1))"
               @keydown.prevent.left="moveLeft($event)"
               @keydown.prevent.right="moveRight($event)"
-              class="score-input p-0 bg-transparent border-0 focus:scale-125"
+              class="score-input text-left"
               type="text" />
           </td>
-        </template>
+        </div>
+        <td>
+          <input
+            data-col="1"
+            :data-row="rowIndex"
+            :value="student.nisn"
+            @keydown.prevent.up="moveUp($event)"
+            @keydown.prevent.down="moveDown($event, (rowIndex === students.length - 1))"
+            @keydown.prevent.left="moveLeft($event)"
+            @keydown.prevent.right="moveRight($event)"
+            class="score-input w-full text-center"
+            type="text" />
+        </td>
+        <td>
+          <input
+            data-col="2"
+            :data-row="rowIndex"
+            value="L"
+            @keydown.prevent.up="moveUp($event)"
+            @keydown.prevent.down="moveDown($event, (rowIndex === students.length - 1))"
+            @keydown.prevent.left="moveLeft($event)"
+            @keydown.prevent.right="moveRight($event)"
+            class="score-input w-full text-center"
+            type="text" />
+        </td>
+        <td>
+          <input
+            data-col="3"
+            :data-row="rowIndex"
+            value="Pasuruan"
+            @keydown.prevent.up="moveUp($event)"
+            @keydown.prevent.down="moveDown($event, (rowIndex === students.length - 1))"
+            @keydown.prevent.left="moveLeft($event)"
+            @keydown.prevent.right="moveRight($event)"
+            class="score-input w-full text-center"
+            type="text" />
+        </td>
+        <td>
+          <input
+            data-col="4"
+            :data-row="rowIndex"
+            value="2001-05-04"
+            @keydown.prevent.up="moveUp($event)"
+            @keydown.prevent.down="moveDown($event, (rowIndex === students.length - 1))"
+            @keydown.prevent.left="moveLeft($event)"
+            @keydown.prevent.right="moveRight($event)"
+            class="score-input w-full text-center"
+            type="text" />
+        </td>
+        <td>
+          <input
+            data-col="5"
+            :data-row="rowIndex"
+            value="Islam"
+            @keydown.prevent.up="moveUp($event)"
+            @keydown.prevent.down="moveDown($event, (rowIndex === students.length - 1))"
+            @keydown.prevent.left="moveLeft($event)"
+            @keydown.prevent.right="moveRight($event)"
+            class="score-input w-full text-center"
+            type="text" />
+        </td>
+        <td>
+          <input
+            data-col="6"
+            :data-row="rowIndex"
+            value="Pilangsari RT 02 RW 10 Desa Beji Kec. Beji"
+            @keydown.prevent.up="moveUp($event)"
+            @keydown.prevent.down="moveDown($event, (rowIndex === students.length - 1))"
+            @keydown.prevent.left="moveLeft($event)"
+            @keydown.prevent.right="moveRight($event, true)"
+            class="score-input text-center"
+            type="text" />
+        </td>
 
-        <template #body-center="{ item, lastRow, rowIndex }">
-          <td>
-            <input
-              data-col="1"
-              :data-row="rowIndex"
-              :value="item.nisn"
-              @keydown.prevent.up="moveUp($event)"
-              @keydown.prevent.down="moveDown($event, lastRow)"
-              @keydown.prevent.left="moveLeft($event)"
-              @keydown.prevent.right="moveRight($event)"
-              class="score-input w-full text-center p-0 bg-transparent border-0 focus:scale-125"
-              type="text" />
-          </td>
-          <td>
-            <input
-              data-col="2"
-              :data-row="rowIndex"
-              value="L"
-              @keydown.prevent.up="moveUp($event)"
-              @keydown.prevent.down="moveDown($event, lastRow)"
-              @keydown.prevent.left="moveLeft($event)"
-              @keydown.prevent.right="moveRight($event, true)"
-              class="score-input w-full text-center p-0 bg-transparent border-0 focus:scale-125"
-              type="text" />
-          </td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-        </template>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <div class="freeze">
+          <td class="w-8 bg-yellow-300">{{ parseInt(Math.random() * 3) }}</td>
+          <td class="w-8 bg-yellow-300">{{ parseInt(Math.random() * 3) }}</td>
+          <td class="w-8 bg-yellow-300">{{ parseInt(Math.random() * 3) }}</td>
+        </div>
+      </tr>
+    </template>
+  </template>
+</Frozen-Table>
 
-        <template #body-end="{ item }">
-          <td class="w-8">{{ parseInt(Math.random() * 3) }}</td>
-          <td class="w-8">{{ parseInt(Math.random() * 3) }}</td>
-          <td class="w-8">{{ parseInt(Math.random() * 3) }}</td>
-        </template>
-      </Frozen-Table>
-    </div>
-  </div>
-</div>
-
-</section>
 </template>
 
 <style scoped>
 
-.table-wrapper::-webkit-scrollbar {
-  @apply hidden;
-}
-
-thead th {
-  @apply border-r border-violet-500;
-}
-
-thead th,
-tbody div > td {
-  @apply px-2 py-1 whitespace-nowrap;
-}
-
 td {
   @apply border-r border-slate-400;
-}
-
-thead th {
-  @apply bg-violet-300;
 }
 
 thead th.parent {
   @apply bg-lime-200;
 }
 
-thead th.presence {
-  @apply bg-yellow-300;
-}
-
-tbody tr > td {
-  @apply bg-sky-200 cursor-pointer hover:bg-sky-400 transition duration-300;
-}
-
 td input::-webkit-inner-spin-button {
   @apply hidden;
+}
+
+td input.score-input {
+  @apply p-0 bg-transparent border-0 focus:font-bold;
 }
 
 td input.score-input:focus {
@@ -171,6 +199,10 @@ td input.score-input:focus {
 
 td:has(input:focus) {
   @apply bg-rose-200;
+}
+
+tbody tr:has(td input.address:focus) * {
+  @apply h-20;
 }
 
 </style>
